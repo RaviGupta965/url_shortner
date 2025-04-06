9ac131d7c4ae4b41e46441c2579f0ef7ae502013
RaviGupta965 <clicktoravi1234@gmail.com>
2025-04-06 23:44:22 +0530

    first commit

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const dbconnect=()=>{
     try {
-        mongoose.connect('mongodb+srv://Ravi:Ravi2004@cluster0.j82qs.mongodb.net/'+process.env.DB_NAME)
+        mongoose.connect(process.env.MONGO_URI+process.env.DB_NAME)
         console.log('Database Connected Successfully');
     } catch (error) {
         console.log('ERROR :: ',error);
